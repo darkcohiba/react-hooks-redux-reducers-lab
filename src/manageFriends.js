ef614bbdc6b827fd2708e202053877a1b06e2620
Sam <samuel.waters@gmail.com>
2022-05-15 21:00:09 -0600

    commit tree 5ec53c711946c5a5c6cea2284a59ef301f1aa7b1

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
-export function manageFriends(state, action) {
-  // your code here
+// const friends = [];
+
+export function manageFriends(state = friends, action) {
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends: [...state.friends, action.payload]
+      }
+    case "friends/remove":
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      }
+    default: return state;
+  }
 }
